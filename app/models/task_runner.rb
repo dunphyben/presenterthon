756dfb3f8974681fc155bd205ce3e865d720342c
Jonathan Wrobel <jwrobes@gmail.com>
2014-09-17 15:40:30 -0700

    removed binding pry

--- app/models/task_runner.rb
+++ app/models/task_runner.rb
@@ -2,7 +2,6 @@ class Task_Runner
 
 	def self.send_email(emails)
 		file = Nokogiri::HTML(open('app/views/send_emails/presentation_email.html.erb'))
-		binding.pry
 		Mailjet::MessageDelivery.create(from:"dunphy.ben@gmail.com",to: emails, subject:"testing Api", html: file )
 	end	
 
